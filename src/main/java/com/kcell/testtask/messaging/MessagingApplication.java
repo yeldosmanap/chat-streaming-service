9e86953efbaf7a3d68bfa02c6fe588069433435f
Yeldos Manap <yeldos.manap@gmail.com>
2023-09-12 01:21:18 +0600

    Add initial project with dependencies

---
+++ src/main/java/com/kcell/testtask/messaging/MessagingApplication.java
@@ -0,0 +1,13 @@
+package com.kcell.testtask.messaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingApplication.class, args);
+	}
+
+}
