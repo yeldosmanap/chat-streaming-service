ef9f444373d5177d8ecbc6d6a73030177ec7d465
Yeldos Manap <yeldos.manap@gmail.com>
2023-09-12 14:50:39 +0600

    Added Message Sender Service and its implementation for Kafka Producer

---
+++ src/main/java/com/kcell/testtask/messaging/service/implementations/KafkaProducerMessageSenderService.java
@@ -0,0 +1,20 @@
+package com.kcell.testtask.messaging.service.implementations;
+
+import com.kcell.testtask.messaging.kafka.KafkaProducer;
+import com.kcell.testtask.messaging.model.Message;
+import com.kcell.testtask.messaging.service.MessageSenderService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducerMessageSenderService implements MessageSenderService {
+    private final KafkaProducer kafkaProducer;
+
+    public KafkaProducerMessageSenderService(KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @Override
+    public void sendMessage(Message message) {
+        kafkaProducer.sendMessage(message);
+    }
+}
