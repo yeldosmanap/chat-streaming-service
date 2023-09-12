989dece733d3555a59b8000f38fa9ca3ea85b89b
Yeldos Manap <yeldos.manap@gmail.com>
2023-09-13 01:44:42 +0600

    Add User entity

---
+++ src/main/java/com/kcell/testtask/messaging/model/User.java
@@ -0,0 +1,23 @@
+package com.kcell.testtask.messaging.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+}
