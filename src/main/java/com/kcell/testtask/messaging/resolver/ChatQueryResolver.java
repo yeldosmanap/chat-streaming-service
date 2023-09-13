2e67d94dbe598d6160c3981c7cd5f74581baf00d
Yeldos Manap <yeldos.manap@gmail.com>
2023-09-14 05:55:19 +0600

    Refactore ChatQueryResolver by adding PreAuthorize annotation and changing fetching method

--- src/main/java/com/kcell/testtask/messaging/resolver/ChatQueryResolver.java
+++ src/main/java/com/kcell/testtask/messaging/resolver/ChatQueryResolver.java
@@ -3,21 +3,21 @@ package com.kcell.testtask.messaging.resolver;
 import com.kcell.testtask.messaging.model.Message;
 import com.kcell.testtask.messaging.service.MessageService;
 import graphql.kickstart.tools.GraphQLQueryResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class ChatQueryResolver implements GraphQLQueryResolver {
     private final MessageService messageService;
 
-    public ChatQueryResolver(MessageService messageService) {
-        this.messageService = messageService;
-    }
-
+    @PreAuthorize("hasRole('USER')")
     @QueryMapping
     public List<Message> messages() {
-        return messageService.getMessages();
+        return messageService.getMessagesWithAuthor();
     }
 }
