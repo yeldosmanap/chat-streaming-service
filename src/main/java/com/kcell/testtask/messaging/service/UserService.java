1a338210dd99aa6e8d9bb6f0eed5737e975ba44d
Yeldos Manap <yeldos.manap@gmail.com>
2023-09-14 05:33:56 +0600

    Refactored code, removed unnecessary files

--- src/main/java/com/kcell/testtask/messaging/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.kcell.testtask.messaging.service;
-
-import com.kcell.testtask.messaging.dto.request.UserCreateRequestDto;
-import com.kcell.testtask.messaging.dto.response.UserResponseDto;
-
-public interface UserService {
-    UserResponseDto saveUser(UserCreateRequestDto request);
-    UserResponseDto deleteUserById(Long id);
-}
