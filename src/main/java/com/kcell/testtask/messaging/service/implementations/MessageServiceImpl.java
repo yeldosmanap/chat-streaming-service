910b5617ad2f8b1dc5e0d4219a703778f44a6144
Yeldos Manap <yeldos.manap@gmail.com>
2023-09-14 05:54:02 +0600

    Create getMessagesWithAuthor method in MessageService

--- src/main/java/com/kcell/testtask/messaging/service/implementations/MessageServiceImpl.java
+++ src/main/java/com/kcell/testtask/messaging/service/implementations/MessageServiceImpl.java
@@ -3,20 +3,23 @@ package com.kcell.testtask.messaging.service.implementations;
 import com.kcell.testtask.messaging.model.Message;
 import com.kcell.testtask.messaging.repository.MessageRepository;
 import com.kcell.testtask.messaging.service.MessageService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class MessageServiceImpl implements MessageService {
     private final MessageRepository messageRepository;
-
-    public MessageServiceImpl(MessageRepository messageRepository) {
-        this.messageRepository = messageRepository;
-    }
 
     @Override
     public List<Message> getMessages() {
         return messageRepository.findAll();
+    }
+
+    @Override
+    public List<Message> getMessagesWithAuthor() {
+        return messageRepository.findAllBy();
     }
 }
