b8dacf759198abd93185aac0954073627443660b
Yeldos Manap <yeldos.manap@gmail.com>
2023-09-14 15:49:33 +0600

    Annotate TokenRepository with @Repository annotation

--- src/main/java/com/kcell/testtask/messaging/repository/TokenRepository.java
+++ src/main/java/com/kcell/testtask/messaging/repository/TokenRepository.java
@@ -3,10 +3,12 @@ package com.kcell.testtask.messaging.repository;
 import com.kcell.testtask.messaging.model.Token;
 import com.kcell.testtask.messaging.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface TokenRepository extends JpaRepository<Token, Long> {
     List<Token> findAllValidTokenByUser(User user);
 
